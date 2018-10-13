a0c722970166a53fb3f5e085519d91a2fb34d967
sergit <beginaviz@gmail.com>
2018-10-13 19:35:45 +0200

    random setup and basic interactive turn for two players

---
+++ main.js
@@ -0,0 +1,116 @@
+var patches = [ 
+	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+	11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+	21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+	31, 32, 33 
+];
+
+var patches_selected = new Array();
+
+var selected = "";
+
+main();
+
+function main(){
+	patches = shuffle(patches);
+	drawSetup();	
+}
+
+function drawSetup(){
+	var setup = "";
+	var isSelectionDrawed = false;
+	var count_get_button = 0;
+
+	setup += "<input type='button'  onclick='pass()' value='Pasar turno' /><br />";
+
+	for(i in patches){
+		if(count_get_button > 2){
+			setup += "<img id='" + patches[i] + "' src='img/" + patches[i] + ".png' />";
+		}else{
+			setup += "<img id='" + patches[i] + "' onclick='getPatch(this)' src='img/" + patches[i] + ".png' />";
+		}
+
+		count_get_button++;
+
+		if((i+1) % 3 === 0){
+			setup += "<br />";
+
+			if(!isSelectionDrawed){
+				setup += "<hr class='separation' />";
+				isSelectionDrawed = true;
+			}
+		}
+	}
+
+	$('body').html(setup);
+}
+
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function getPatch(element){
+	if(selected){
+		$(selected).css({ opacity: 100 });
+	}
+
+	selected = element;
+
+	$(element).css({ opacity: 0.25 });
+}
+
+function pass(){
+	if(selected){
+		patches_selected.push(selected.id);
+
+		var kill_id = -1;
+
+		for(x in patches){
+			if(patches[x] == selected.id){
+				kill_id = x;
+			}
+		}
+
+		if(kill_id > -1){
+			for(i = 0; i < kill_id; i++){
+				// llevamos el elemento uno o dos al final de los parches
+				patches.push(patches[i]);
+				delete patches[i];
+			}
+
+			delete patches[kill_id];
+		}
+	}else{
+		alert("Selecciona un parche");
+	}
+
+	fixSort();
+	drawSetup();
+}
+
+function fixSort(){
+	var new_patches = [];
+
+	for(x in patches){
+		new_patches.push(patches[x])
+	}
+
+	patches = [];
+	patches = new_patches;
+}
